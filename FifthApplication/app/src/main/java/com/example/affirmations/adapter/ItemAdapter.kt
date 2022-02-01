68f12768a76eb344c32d998982065d1813bdbd63
talout-chattah <taloutchattah2001@gmail.com>
2022-02-01 12:46:00 +0100

    display scorollable list using RecyclerView

---
+++ FifthApplication/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.affirmations.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+
+/**
+ * Adapter for the RecyclerView in MainActivity. Displays Affirmation data object.
+ */
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>
+                 ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    /**
+     * Provide a reference to the view for each data item
+     * Complex data items may need more then one view per item
+     * each data item is just an Affirmation object
+     */
+    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view){
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    /**
+     * Create new views (invoked by the layout manager)
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        //Create new view
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+        return ItemViewHolder(adapterLayout)
+    }
+
+    /**
+     * Replace the contents of a view (invoked by the layout manager)
+     */
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+    }
+
+    /**
+     * Return the size of the dataset (invoked by the layout manager)
+     */
+    override fun getItemCount() = dataset.size
+
+}
