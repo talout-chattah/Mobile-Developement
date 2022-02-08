2739efbc15656d50dfab68f9d92199915d2783c5
talout-chattah <taloutchattah2001@gmail.com>
2022-02-08 23:08:33 +0100

    test successed

--- SixthApplication/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/dogglers/databinding/GridListItemBinding.java
+++ SixthApplication/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/dogglers/databinding/GridListItemBinding.java
@@ -4,6 +4,8 @@ package com.example.dogglers.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewbinding.ViewBinding;
@@ -11,13 +13,31 @@ import com.example.dogglers.R;
 import com.google.android.material.card.MaterialCardView;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class GridListItemBinding implements ViewBinding {
   @NonNull
   private final MaterialCardView rootView;
 
-  private GridListItemBinding(@NonNull MaterialCardView rootView) {
+  @NonNull
+  public final ImageView imageView;
+
+  @NonNull
+  public final TextView textAge;
+
+  @NonNull
+  public final TextView textHobbies;
+
+  @NonNull
+  public final TextView textName;
+
+  private GridListItemBinding(@NonNull MaterialCardView rootView, @NonNull ImageView imageView,
+      @NonNull TextView textAge, @NonNull TextView textHobbies, @NonNull TextView textName) {
     this.rootView = rootView;
+    this.imageView = imageView;
+    this.textAge = textAge;
+    this.textHobbies = textHobbies;
+    this.textName = textName;
   }
 
   @Override
@@ -43,10 +63,38 @@ public final class GridListItemBinding implements ViewBinding {
 
   @NonNull
   public static GridListItemBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.imageView;
+      ImageView imageView = rootView.findViewById(id);
+      if (imageView == null) {
+        break missingId;
+      }
 
-    return new GridListItemBinding((MaterialCardView) rootView);
+      id = R.id.textAge;
+      TextView textAge = rootView.findViewById(id);
+      if (textAge == null) {
+        break missingId;
+      }
+
+      id = R.id.textHobbies;
+      TextView textHobbies = rootView.findViewById(id);
+      if (textHobbies == null) {
+        break missingId;
+      }
+
+      id = R.id.textName;
+      TextView textName = rootView.findViewById(id);
+      if (textName == null) {
+        break missingId;
+      }
+
+      return new GridListItemBinding((MaterialCardView) rootView, imageView, textAge, textHobbies,
+          textName);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
