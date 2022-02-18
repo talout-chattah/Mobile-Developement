9b8b95dae83dc43c38b06f70633b4c4013d19817
talout-chattah <taloutchattah2001@gmail.com>
2022-02-18 22:50:43 +0100

    delete unuseful files

--- DessertClicker/app/build/generated/data_binding_trigger/debug/com/example/android/dessertclicker/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.android.dessertclicker;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- DessertClicker/app/build/generated/source/kapt/debug/com/example/android/dessertclicker/BR.java
+++
@@ -1,9 +0,0 @@
-package com.example.android.dessertclicker;
-
-public class BR {
-  public static final int _all = 0;
-
-  public static final int amountSold = 1;
-
-  public static final int revenue = 2;
-}
