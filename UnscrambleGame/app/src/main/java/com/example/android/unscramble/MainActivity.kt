3feba20f7f3cae1b0962ffd37375ca0a6afbaf81
talout-chattah <taloutchattah2001@gmail.com>
2022-02-19 21:31:45 +0100

    basic interface for the game

---
+++ UnscrambleGame/app/src/main/java/com/example/android/unscramble/MainActivity.kt
@@ -0,0 +1,16 @@
+
+package com.example.android.unscramble
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Creates an Activity that hosts the Game fragment in the app
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
