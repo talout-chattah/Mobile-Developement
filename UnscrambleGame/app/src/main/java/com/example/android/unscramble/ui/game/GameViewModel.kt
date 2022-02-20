741e65c0dc8fd4f28c9931119220c943e4cdb525
talout-chattah <taloutchattah2001@gmail.com>
2022-02-20 22:13:50 +0100

    the logique of making new scrambled game added

--- UnscrambleGame/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ UnscrambleGame/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -5,20 +5,62 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
-    init {
-        Log.d("GameFragment", "GameViewModel Created!")
-    }
+    private var _score = 0
+    val score: Int
+        get() = _score
 
-    private var score = 0
-    private var currentWordCount = 0
-    private var _currentScrambledWord = "test"
+    private var _currentWordCount = 0
+    val currentWordCount: Int
+        get() = _currentWordCount
 
+    private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
+
+    //List of the words used in the game
+    private var wordList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    init {
+        Log.d("GameFragment", "GameViewModel Created!")
+        getNextWord()
+    }
 
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+
+    // Update the current word and the current scrambled word with nest word
+    private fun getNextWord(){
+
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (String(tempWord).equals(currentWord, false)){
+            tempWord.shuffle()
+        }
+
+        if(wordList.contains(currentWord)){
+            getNextWord()
+        }else{
+            _currentScrambledWord = String(tempWord)
+            ++_currentWordCount
+            wordList.add(currentWord)
+        }
+    }
+
+    /*
+    * Return TRUE if the current word count is less then MAX_NO_OF_WORDS
+    * then updates the next word
+    */
+    fun nextWord(): Boolean{
+        return if (currentWordCount < MAX_NO_OF_WORDS){
+            getNextWord()
+            true
+        }else false
     }
 
 }
