b055c516cb9f33499003129eec58bafa1f15191d
talout-chattah <taloutchattah2001@gmail.com>
2022-02-20 22:53:55 +0100

    final score dialog added

--- UnscrambleGame/app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ UnscrambleGame/app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.android.unscramble.R
 import com.example.android.unscramble.databinding.GameFragmentBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 /**
  * Fragment where the game is played, contains the game logic.
@@ -112,5 +113,21 @@ class GameFragment : Fragment() {
      */
     private fun updateNextWordOnScreen() {
         binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord
+    }
+    /*
+     * Creates and shows an AlertDialog with the final score
+     */
+    private fun showFinalScoreDialog(){
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(getString(R.string.congratulations))
+            .setMessage(getString(R.string.you_scored, viewModel.score))
+            .setCancelable(false)
+            .setNegativeButton(getString(R.string.exit)){
+                _, _->exitGame()
+            }
+            .setPositiveButton(getString(R.string.exit)){
+                _,_->restartGame()
+            }
+            .show()
     }
 }
