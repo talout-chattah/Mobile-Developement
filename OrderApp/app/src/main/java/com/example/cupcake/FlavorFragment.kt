c70f50d2c1f71f4cb5c10e110d891d0ab3f40532
talout-chattah <taloutchattah2001@gmail.com>
2022-02-28 22:09:41 +0100

    Navigation graph completed

--- OrderApp/app/src/main/java/com/example/cupcake/FlavorFragment.kt
+++ OrderApp/app/src/main/java/com/example/cupcake/FlavorFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentFlavorBinding
 
 /**
@@ -39,7 +40,7 @@ class FlavorFragment : Fragment() {
      * Navigate to the next screen to choose pickup date.
      */
     fun goToNextScreen() {
-        Toast.makeText(activity, "Next", Toast.LENGTH_SHORT).show()
+        findNavController().navigate(R.id.action_flavorFragment_to_pickupFragment)
     }
 
     /**
--- OrderApp/app/src/main/java/com/example/cupcake/StartFragment.kt
+++ OrderApp/app/src/main/java/com/example/cupcake/StartFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentStartBinding
 
 /**
@@ -42,7 +43,7 @@ class StartFragment : Fragment() {
      * Start an order with the desired quantity of cupcakes and navigate to the next screen.
      */
     fun orderCupcake(quantity: Int) {
-        Toast.makeText(activity, "Ordered $quantity cupcake(s)", Toast.LENGTH_SHORT).show()
+        findNavController().navigate(R.id.action_startFragment_to_flavorFragment)
     }
 
     /**
