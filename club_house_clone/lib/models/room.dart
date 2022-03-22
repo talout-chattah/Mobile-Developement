8bca6673bed0e4664e52a6d20e725236f67abad5
talout-chattah <71949747+talout-chattah@users.noreply.github.com>
2022-03-22 22:24:48 +0100

    models and data file added

---
+++ club_house_clone/lib/models/room.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:club_house_clone/models/user.dart';
+
+class Room {
+  final String club;
+  final String name;
+  final List<User> speakers;
+  final List<User> followedBySpeakers;
+  final List<User> others;
+
+  Room({
+    required this.club,
+    required this.name,
+    this.speakers = const [],
+    this.followedBySpeakers = const [],
+    this.others = const [],
+  });
+}
---
+++ club_house_clone/lib/models/user.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class User {
+  final String firstName;
+  final String lastName;
+  final String imageURL;
+
+  const User(
+      {required this.firstName,
+      required this.lastName,
+      required this.imageURL});
+}
