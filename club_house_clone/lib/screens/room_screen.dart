65bf844b5744877ea543dca0f642a983cb0b230c
talout-chattah <taloutchattah2001@gmail.com>
2022-03-26 21:33:50 +0100

    Speakers part in RoomScreen complited

--- club_house_clone/lib/screens/room_screen.dart
+++ club_house_clone/lib/screens/room_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:club_house_clone/models/room.dart';
+import 'package:club_house_clone/widgets/room_user_profile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -42,6 +45,68 @@ class RoomScreen extends StatelessWidget {
             ),
           )
         ],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(40),
+        ),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '${room.club} 🏠'.toUpperCase(),
+                        style: Theme.of(context).textTheme.overline!.copyWith(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: 1.0,
+                            ),
+                      ),
+                      Icon(
+                        CupertinoIcons.ellipsis,
+                      ),
+                    ],
+                  ),
+                  Text(
+                    room.name,
+                    style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.0,
+                        ),
+                  ),
+                ],
+              ),
+            ),
+            SliverPadding(
+              padding: const EdgeInsets.all(14.0),
+              sliver: SliverGrid.count(
+                crossAxisCount: 3,
+                mainAxisSpacing: 15.0,
+                children: room.speakers
+                    .map(
+                      (e) => RoomUserProfile(
+                        imageURL: e.imageURL,
+                        name: e.firstName,
+                        size: 66,
+                        isNew: Random().nextBool(),
+                        isMuted: Random().nextBool(),
+                      ),
+                    )
+                    .toList(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
